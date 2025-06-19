9d1daa38f59b8090b1a8496394a56687683d09cc
lancelot <github@sus.fish>
2025-06-19 03:29:17 -0400

    add lttng helper script

---
+++ lttng-start.sh
@@ -0,0 +1,9 @@
+mydate=$(date +"%b%d-%H%M" | tr '[:upper:]' '[:lower:]')
+lttng destroy
+lttng create $mydate
+lttng enable-channel --userspace --blocking-timeout=inf blocking-channel
+lttng enable-event -c blocking-channel -u hiptrace*
+lttng add-context -c blocking-channel -u -t vpid -t vtid
+lttng start
+LTTNG_UST_ALLOW_BLOCKING=1 LTTNG_UST_APP_PATH="/home/users/lancend/mestraces" LTTNG_UST_DEBUG=1 LTTNG_UST_VERBOSE=1 LD_PRELOAD="/usr/local/lib/liblttng-ust-libc-wrapper.so:/usr/local/wrap_hip.so" ./hip_async_test
+lttng stop
