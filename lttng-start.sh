32ad16ccfc9e816f34f37657eeee085fb18e0504
lancelot <github@sus.fish>
2025-09-04 10:07:07 -0400

    tmpdisable mallocs

--- lttng-start.sh
+++ lttng-start.sh
@@ -4,10 +4,10 @@ lttng destroy
 lttng create $mydate
 lttng enable-channel --userspace --blocking-timeout=inf blocking-channel
 cd ../
-lttng enable-event -c blocking-channel -u lttng_ust_libc:malloc
-lttng enable-event -c blocking-channel -u lttng_ust_libc:calloc
-lttng enable-event -c blocking-channel -u lttng_ust_libc:free
-lttng enable-event -c blocking-channel -u lttng_ust_libc:realloc 
+#lttng enable-event -c blocking-channel -u lttng_ust_libc:malloc
+#lttng enable-event -c blocking-channel -u lttng_ust_libc:calloc
+#lttng enable-event -c blocking-channel -u lttng_ust_libc:free
+#lttng enable-event -c blocking-channel -u lttng_ust_libc:realloc 
 lttng enable-event -c blocking-channel -u hiptrace*
 cd -
 lttng add-context -c blocking-channel -u -t vpid -t vtid
