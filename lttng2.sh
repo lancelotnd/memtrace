7252b93ec69fefe7981eb9bf1a76259de01bc0fb
lancelot <github@sus.fish>
2025-09-04 11:00:26 -0400

    newv

---
+++ lttng2.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+conda activate unidev
+mydate=$(date +"%b%d-%H%M" | tr '[:upper:]' '[:lower:]')
+
+# Ensure a clean sessiond, then let lttng auto-spawn it with default ~/.lttng
+pkill -u "$USER" -f lttng-sessiond || true
+unset LTTNG_UST_APP_PATH
+unset LTTNG_HOME
+
+lttng destroy || true
+lttng create "$mydate"
+cd ..
+lttng enable-channel --userspace --blocking-timeout=inf blocking-channel
+lttng enable-event -c blocking-channel -u "hiptrace:*"
+cd -
+lttng add-context -c blocking-channel -u -t vpid -t vtid
+lttng start
+
+cd ~/pytorch
+LTTNG_UST_ALLOW_BLOCKING=1 LTTNG_UST_APP_PATH="/home/users/lancend/mestraces" LTTNG_UST_DEBUG=1 LTTNG_UST_VERBOSE=1 LD_PRELOAD="/usr/local/lib/liblttng-ust-libc-wrapper.so:/usr/local/wrap_hip.so" python ~/f5.py --model llama --train_batch_size=1 --eval_batch_size 1 --num_train_samples=1 --name test1
+lttng stop
+ctf2ctf ~/lttng-traces/$mydate* --output ~/code/S25-Experiments/memtrace/traces/$mydate.json 2>/dev/null
+
+#python ~/code/S25-Experiments/memtrace/process_trace.py ~/code/S25-Experiments/memtrace/traces/$mydate_tmp.json ~/code/S25-Experiments/memtrace/traces/$mydate.json
+
